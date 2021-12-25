76c2b1c8bbfdc2bd478f1e90f29ed86ecca7df9e
monotera <monotera@pop-os.localdomain>
2021-12-25 18:46:02 -0500

    finishes guess the light game

--- guessTheLight.ino
+++ guessTheLight.ino
@@ -1,31 +1,123 @@
+//Pin constants
+const int gameButtonPin = 2;
+const int initButtonPin = 3;
+const int redLedPin = 4;
+const int blueLedPin = 5;
+const int yellowLedPin = 6;
+const int piezoPin = 8;
+const int rgbRedPin = 9;
+const int rgbGreenPin = 10;
+const int rgbBluePin = 11;
 
-int turnLightOn(int light);
+void loading();
+void game();
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(7, INPUT);
+  pinMode(redLedPin, OUTPUT);
+  pinMode(blueLedPin, OUTPUT);
+  pinMode(yellowLedPin, OUTPUT);
+  pinMode(initButtonPin, INPUT);
+  pinMode(gameButtonPin, INPUT);
+  pinMode(rgbRedPin, OUTPUT);
+  pinMode(rgbGreenPin, OUTPUT);
+  pinMode(rgbBluePin, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  int btnValue = digitalRead(7); 
-  static bool isLightOn = false; //SE REALIZAR LECTURA DIGITAL PIN 7
-  delay(1);
-  int randNumber = random(2,5);
-  if(btnValue == 1)
-  {
-    turnLightOn(randNumber);
+  analogWrite(rgbRedPin, map(0, 0, 1023, 0, 255));
+  analogWrite(rgbGreenPin, map(0, 0, 1023, 0, 255));
+  analogWrite(rgbBluePin, map(128, 0, 1023, 0, 255));
+  int initButtonState = digitalRead(initButtonPin);
+  int initLastButtonState = 0;
+  if (initButtonState != initLastButtonState) {
+    if (initButtonState == HIGH) {
+      Serial.println("Start game function");
+      int randNumber = random(4, 7);
+      game(randNumber);
+    } else {
+      Serial.println("End game");
+    }
+    delay(50);
+  }
+  initLastButtonState = initButtonState;
+  loading();
+}
+
+
+void game(int randNumber) {
+  delay(500);
+  int initButtonState = 0;
+  int buttonPushCounter = 0; 
+  Serial.print("Game has started, the random number is: ");
+  Serial.println(randNumber);
+  int gameLastButtonState = 0;
+
+  do {
+    initButtonState = 0;
+    int gameButtonState = digitalRead(gameButtonPin);
+    delay(100);
+    if (gameButtonState != gameLastButtonState) {
+      if (gameButtonState == HIGH) {
+        buttonPushCounter++;
+        Serial.print("number of button pushes: ");
+        Serial.println(buttonPushCounter);
+      }
+      delay(50);
+    }
+    initButtonState = digitalRead(initButtonPin);
+    gameLastButtonState = gameButtonState;
+  } while (initButtonState == 0);
+
+  if (buttonPushCounter >= 1 && buttonPushCounter <= 3) {
+    if (buttonPushCounter + 3 == randNumber) {
+      Serial.println("You won!");
+      analogWrite(rgbRedPin, map(0, 0, 1023, 0, 255));
+      analogWrite(rgbGreenPin, map(202, 0, 1023, 0, 255));
+      analogWrite(rgbBluePin, map(0, 0, 1023, 0, 255));
+      digitalWrite(randNumber, HIGH);
+      tone(piezoPin, 1500, 100);
+      delay(300);
+      tone(piezoPin, 1000, 100);
+      delay(300);
+      tone(piezoPin, 800, 100);
+      delay(1000);
+      digitalWrite(randNumber, LOW);
+    } else {
+      Serial.println("You loose!");
+      analogWrite(rgbRedPin, map(202, 0, 1023, 0, 255));
+      analogWrite(rgbGreenPin, map(0, 0, 1023, 0, 255));
+      analogWrite(rgbBluePin, map(0, 0, 1023, 0, 255));
+      digitalWrite(randNumber, HIGH);
+      digitalWrite(buttonPushCounter + 3, HIGH);
+      for (int i = 0; i < 2; i++) {
+        tone(piezoPin, 100, 800);
+        delay(1000);
+      }
+      delay(1000);
+      digitalWrite(randNumber, LOW);
+      digitalWrite(buttonPushCounter + 3, LOW);
+    }
+  } else {
+    Serial.println("Error!");
+    analogWrite(rgbRedPin, map(255, 0, 1023, 0, 255));
+    analogWrite(rgbGreenPin, map(128, 0, 1023, 0, 255));
+    tone(piezoPin, 300, 100);
+    delay(300);
+    tone(piezoPin, 100, 100);
+    delay(300);
+    tone(piezoPin, 500, 100);
+    delay(2000);
   }
 }
-
-int turnLightOn(int light) {
-  Serial.println(light);
-  digitalWrite(light, HIGH);
-  delay(1000);
-  digitalWrite(light, LOW);
+void loading() {
+  digitalWrite(blueLedPin, HIGH);
+  delay(50);
+  digitalWrite(blueLedPin, LOW);
+  digitalWrite(redLedPin, HIGH);
+  delay(60);
+  digitalWrite(redLedPin, LOW);
+  digitalWrite(yellowLedPin, HIGH);
+  delay(70);
+  digitalWrite(yellowLedPin, LOW);
 }
-
